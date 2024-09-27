9fb6279b64ab321480e055d3ca2a8a56a14ac4df
Banna150149 <mdhasanalbanna@tamu.edu>
2024-09-26 21:36:10 -0500

    Added new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,11 +26,10 @@
 # end
 
 more_movies = [
-  { title: '3 Idiots', rating: 'G', release_date: '16-Apr-2020' },
-  { title: 'Green Book', rating: 'PG-13', release_date: '16-Nov-2018' },
-  { title: 'Parasite', rating: 'R', release_date: '30-May-2019' },
-  { title: 'Nomadland', rating: 'R', release_date: '19-Feb-2021' },
-  { title: 'CODA', rating: 'PG-13', release_date: '13-Aug-2021' }
+  { :title =>'My Neighbor Totoro', :rating=> 'G', :release_date => '16-Apr-1988'},
+  { :title =>'Aynabaji', :rating=> 'R', :release_date=> '16-Nov-2020' },
+  { :title =>'Abar Boshonto', :rating=> 'R', :release_date=> '20-May-2017' },
+  { :title => 'Tufan', :rating=> 'PG-13', :release_date=> '12-Jun-2024' }
 ]
 
 more_movies.each do |movie|
